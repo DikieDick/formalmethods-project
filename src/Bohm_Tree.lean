ca2f2438567a7d7c157fcb918559d5a30a930186
NootvdBeuk <80099691+NootvdBeuk@users.noreply.github.com>
2026-04-24 18:12:37 +0200

    Add lemmas 3.5 and 3.7, together with the definitions required to do so

--- src/Bohm_Tree.lean
+++ src/Bohm_Tree.lean
@@ -1,4 +1,5 @@
 import Cslib.Languages.LambdaCalculus.LocallyNameless.Untyped.Basic
+import Cslib.Languages.LambdaCalculus.LocallyNameless.Untyped.FullBeta
 import Mathlib.Tactic
 
 open Cslib
@@ -25,6 +26,7 @@ inductive isHeadNormal : Term Var → Prop where
   | base T : isHeadNormalApp T -> isHeadNormal T
   | step T : isHeadNormal T -> isHeadNormal (Term.abs T)
 
+-- Lemma 3.2
 lemma normal_or_redex (T : Term Var) : isHeadRedex T ∨ isHeadNormal T := by
   induction' T with T T T ih T₁ T₂ ih1 ih2
   · right
@@ -43,3 +45,38 @@ lemma normal_or_redex (T : Term Var) : isHeadRedex T ∨ isHeadNormal T := by
     · rcases ih1 with ih1 | ih1
       · cases ih1 ; left ; apply isHeadRedex.base ; apply isHeadRedexApp.step ; assumption
       · cases ih1 ; right ; apply isHeadNormal.base ; apply isHeadNormalApp.step ; assumption
+
+def nfoldAbs : ℕ → Term Var → Term Var
+| 0, T => T
+| n + 1, T => nfoldAbs n T.abs
+
+def nfoldApp : List (Term Var) → Term Var → Term Var
+| [], T => T
+| a :: as, T => nfoldApp as (T.app a)
+
+-- Placeholder for now
+inductive isBetaEquiv : Term Var → Term Var → Prop where
+
+inductive betaEquivList : List (Term Var) → List (Term Var) → Prop where
+  | base : betaEquivList [] []
+  | step as bs T₁ T₂ : isBetaEquiv T₁ T₂ → betaEquivList as bs → betaEquivList (T₁ :: as) (T₂ :: bs)
+
+-- Reduction does not affect the number of leading abstractions, the head variable, or the number of arguments of the head-variable
+lemma reduction_preservation (n m : ℕ) (y z : Var) (Ps Qs : List (Term Var)) :
+    Term.Beta (nfoldAbs n (nfoldApp Ps (Term.fvar y))) (nfoldAbs m (nfoldApp Qs (Term.fvar z))) ->
+    n = m ∧ y = z ∧ betaEquivList Ps Qs := by
+  sorry
+
+-- Lemma 3.5
+lemma hnfs_similar (n m : ℕ) (y z : Var) (Ps Qs : List (Term Var)) (M : Term Var) :
+    isBetaEquiv M (nfoldAbs n (nfoldApp Ps (Term.fvar y))) ->
+    isBetaEquiv M (nfoldAbs m (nfoldApp Qs (Term.fvar z))) ->
+    n = m ∧ y = z ∧ betaEquivList Ps Qs := by
+  sorry
+
+-- coinductive BöhmTree : Type where
+--   | no_hnf : BöhmTree
+--   | hnf : List BöhmTree → BöhmTree
+--
+-- -- Definition 3.7
+-- def BT : Term Var → BöhmTree
