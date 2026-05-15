7ff41033256b4eafe06824276fab6400bc382ec9
DikieDick <26772815+DikieDick@users.noreply.github.com>
2026-05-15 17:20:36 +0200

    Add helper lemmas

--- src/Bohm_Tree.lean
+++ src/Bohm_Tree.lean
@@ -124,6 +124,13 @@ def BöhmTree.hnf (n : ℕ) (na : ℕ) (t : bfvar Var) (f : ULift (Fin n) → (B
 def hasAsHnf : Term Var → (n : ℕ) → List (Term Var) → bfvar Var → Prop := λ T n Ps y ↦
   let T' := nfoldAbs n (nfoldApp Ps y.val) ; isHeadNormal T' ∧ T.BetaEquiv T'
 
+
+lemma HnfEq_of_BetaEquiv (M N : Term Var) (hMN : M.BetaEquiv N) n Ps y (h : hasAsHnf M n Ps y) : hasAsHnf N n Ps y := by
+  unfold hasAsHnf at *; dsimp at *
+  obtain ⟨hl, hr⟩ := h
+  refine ⟨hl, ?_⟩
+  apply Relation.EqvGen.trans N M _ hMN.symm hr
+
 -- Definition 3.7
 coinductive BT (Var : Type u) : Term Var → BöhmTree Var → Prop where
   | no_hnf (T : Term Var) :
