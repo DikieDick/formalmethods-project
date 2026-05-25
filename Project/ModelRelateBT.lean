58327b43a571add841c84232f2f988eac7f48e43
User <user@Users-MacBook-Air.local>
2026-05-26 01:54:07 +0200

    Added comments

--- Project/ModelRelateBT.lean
+++ Project/ModelRelateBT.lean
@@ -18,21 +18,23 @@ variable {σ : Var → Set α}
 
 def solvable (T : Term Var) : Prop := sorry -- Has to be defined
 
-theorem Interp_iff_context_solvable (A B : Term Var):
+-- lemmas needed to prove our final theorem
+lemma Interp_iff_context_solvable (A B : Term Var):
 〚 A 〛_{ρ,σ} = 〚 B 〛_{ρ,σ} ↔ ∀ C : Term Var -> Term Var, (solvable (C A) ↔  solvable (C B))
   := by
   sorry
 
-theorem solvable_iff_hasHnf (A: Term Var):
+lemma solvable_iff_hasHnf (A: Term Var):
 solvable A ↔ hasHnf A
   := by
   sorry
 
-theorem context_solvable_iff_BT (A B : Term Var):
+lemma context_solvable_iff_BT (A B : Term Var):
 ( ∀ C : Term Var -> Term Var, (solvable (C A) ↔  solvable (C B))) ↔ BT A = BT B
   := by
   sorry
 
+-- Our final theorem we wanted to state
 theorem BT_eq_Interp (A B : Term Var):
 〚 A 〛_{ρ,σ} = 〚 B 〛_{ρ,σ} ↔ BT A = BT B
   := by
